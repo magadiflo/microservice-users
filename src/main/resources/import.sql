3eae5a3d61190d2137bd5fb2b0954910bff43a32
Martín <magadiflo@gmail.com>
2022-11-07 20:15:17 -0500

    Agregando contraseñas encriptadas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users(username, password, enabled, name, last_name, email) VALUES('magadiflo', '12345', true, 'Martín', 'Díaz', 'magadiflo@magadiflo.com');
-INSERT INTO users(username, password, enabled, name, last_name, email) VALUES('admin', '12345', true, 'Admin', 'Admin', 'admin@magadiflo.com');
+INSERT INTO users(username, password, enabled, name, last_name, email) VALUES('magadiflo', '$2a$10$gVgmPrXYfivKf55bJULutOO.dT54Rju.wLbV72gHUgvXerZ6nP0BK', true, 'Martín', 'Díaz', 'magadiflo@magadiflo.com');
+INSERT INTO users(username, password, enabled, name, last_name, email) VALUES('admin', '$2a$10$BlXyOfhh9ntQvjwgMu2BsO0qXHr.Avdvm/takg5qc4KAkIhqVY4CG', true, 'Admin', 'Admin', 'admin@magadiflo.com');
 
 INSERT INTO roles(name) VALUES('ROLE_USER'); 
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
